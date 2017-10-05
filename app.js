cb8620e5b46ac87cd39bcd1861cfa10b878c4926
Felipe Lopes <felipem_lopes@hotmail.com>
2017-10-04 22:01:29 -0300

    ✨ add ORM Sequelize for the server

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('ARANDU', 'root', '', {dialect: 'mysql'})
+
+const Disciplina = sequelize.define('Disciplina', {
+  Nome: {
+    type: Sequelize.STRING
+  }
 })
+
+app.listen(3000)
 
 app.get('/', function (req, res) {
   res.json({ user: 'Felipe' })
+})
+
+app.get('/criar', function (req, res) {
+  sequelize.sync()
+    .then(() => Disciplina.create({
+      Nome: 'Matemática'
+    }))
+    .then(jane => {
+      res.json({ user: 'Felipe' })
+    });
 })
