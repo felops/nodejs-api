915200c1612f9dce309964e0f11a0b7cb369df76
felops <felops@users.noreply.github.com>
2017-10-11 15:02:16 -0300

    ✅ move fake data to another file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const models = require('./models')
+const mockData = require('./mock-data')
 
 app.use(express.json());
 app.use(function(req, res, next) {
@@ -11,6 +12,9 @@ app.use(function(req, res, next) {
 });
 
 models.sequelize.sync().then(()  => {
+  // mock data for testing
+  mockData(models)
+
   // custom routes
   app.get('/api/loadExam/:id', (req, res) => {
     models.entity['ExamQuestion'].findAll({
@@ -57,66 +61,4 @@ models.sequelize.sync().then(()  => {
   }
 
   app.listen(3000);
-
-  // add fake data for testing
-  models.entity['Professor'].create({
-    name: 'Fernando',
-    email: 'fernando@gmail.com',
-    password: 'senha123'
-  })
-
-  models.entity['Discipline'].create({name:'Matemática'})
-  models.entity['Discipline'].create({name:'Português'})
-  models.entity['Discipline'].create({name:'História'})
-
-  models.entity['Class'].create({name:'1A'})
-  models.entity['Class'].create({name:'1B'})
-  models.entity['Class'].create({name:'2A'})
-  models.entity['Class'].create({name:'2B'})
-
-  models.entity['QuestionSource'].create({source:'ENEM'})
-  models.entity['QuestionSource'].create({source:'FUVEST'})
-
-  models.entity['QuestionLevel'].create({name:'A'})
-  models.entity['QuestionLevel'].create({name:'B'})
-  models.entity['QuestionLevel'].create({name:'C'})
-
-  models.entity['Question'].create({
-    id: 1,
-    question: 'Responda:',
-    year: 2016,
-    level: 2,
-    source: 1
-  });
-
-  models.entity['QuestionOption'].create({
-    option: 'Resposta 1',
-    question: 1
-  });
-
-  models.entity['QuestionOption'].create({
-    option: 'Resposta 2',
-    question: 1
-  });
-
-  models.entity['QuestionOption'].create({
-    option: 'Resposta 3',
-    question: 1
-  });
-
-  models.entity['QuestionOption'].create({
-    option: 'Resposta 4',
-    question: 1
-  });
-
-  models.entity['Exam'].create({
-    class: 1,
-    discipline: 1,
-    professor: 1
-  });
-
-  models.entity['ExamQuestion'].create({
-    exam: 1,
-    question: 1
-  });
 });
---
+++ mock-data.js
@@ -0,0 +1,76 @@
+function mockData(models) {
+  models.entity['Professor'].create({
+    name: 'Fernando',
+    email: 'fernando@gmail.com',
+    password: 'senha123'
+  })
+
+  models.entity['Discipline'].create({name:'Matemática'})
+  models.entity['Discipline'].create({name:'Português'})
+  models.entity['Discipline'].create({name:'História'})
+
+  models.entity['Class'].create({name:'1A'})
+  models.entity['Class'].create({name:'1B'})
+  models.entity['Class'].create({name:'2A'})
+  models.entity['Class'].create({name:'2B'})
+
+  models.entity['QuestionSource'].create({source:'ENEM'})
+  models.entity['QuestionSource'].create({source:'FUVEST'})
+
+  models.entity['QuestionLevel'].create({name:'A'})
+  models.entity['QuestionLevel'].create({name:'B'})
+  models.entity['QuestionLevel'].create({name:'C'})
+
+  for(let i=1; i <= 40; i++) {
+    models.entity['Question'].create({
+      question: 'Responda ' + i,
+      year: 2016,
+      level: 2,
+      source: 1
+    });
+  }
+
+  let j = 1;
+  for(let i=1; i <= 200; i++) {
+    if(i%5 == 0) j++;
+    models.entity['QuestionOption'].create({
+      option: 'Resposta ' + i,
+      question: j
+    });
+  }
+
+  models.entity['Exam'].create({
+    class: 1,
+    discipline: 1,
+    professor: 1
+  });
+
+  models.entity['Exam'].create({
+    class: 2,
+    discipline: 1,
+    professor: 1
+  });
+
+  models.entity['Exam'].create({
+    class: 3,
+    discipline: 1,
+    professor: 1
+  });
+
+  models.entity['Exam'].create({
+    class: 4,
+    discipline: 1,
+    professor: 1
+  });
+
+  j = 1;
+  for(let i=1; i <= 40; i++) {
+    if(i%11 == 0) j++;
+    models.entity['ExamQuestion'].create({
+      exam: j,
+      question: i
+    });
+  }
+}
+
+module.exports = mockData;
