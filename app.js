e0a9d1c4db1274a91105ffa3f0bf619acd5a5176
felops <felops@users.noreply.github.com>
2017-11-08 17:27:12 -0200

    💥 separate login request for professor and student

--- app.js
+++ app.js
@@ -77,11 +77,44 @@ models.sequelize.sync().then(()  => {
 
 
   // POSTs
-  app.post('/api/login', (req, res) => {
-    let post = req.body;
-    let entity = (post.type == 2) ? 'Professor' : 'Student';
+  app.post('/api/student/login', (req, res) => {
+    let post = req.body
 
-    models.entity[entity].findAll({
+    models.entity['Student'].findAll({
+      where: {
+        id: post.id
+      }
+    }).then((data) => {
+      if(data.length == 1) {
+        res.json({
+          data: true,
+          msg: ''
+        });
+      } else {
+        models.entity['Student'].create({ id: post.id, name: post.name, class: post.class }).then(task => {
+          res.json({
+            data: true,
+            msg: ''
+          });
+        }).catch(function(err) {
+          res.json({
+            data: false,
+            msg: err
+          })
+        })
+      }
+    }).catch(function(err) {
+      res.json({
+        data: false,
+        msg: err
+      })
+    })
+  })
+
+  app.post('/api/professor/login', (req, res) => {
+    let post = req.body
+
+    models.entity['Professor'].findAll({
       where: {
         email: post.email,
         password: post.password
