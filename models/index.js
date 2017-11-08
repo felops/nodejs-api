f9baf97623ce938b9c7403a1eba1690fed44f732
felops <felops@users.noreply.github.com>
2017-11-08 17:20:48 -0200

    🔧 identify identity relations for Sequelize

--- models/index.js
+++ models/index.js
@@ -27,4 +27,6 @@ Object.keys(db).forEach(function(modelName) {
 
 db.entity['ExamQuestion'].belongsTo(db.entity['Question'], {foreignKey: 'question'});
 db.entity['Question'].hasMany(db.entity['QuestionOption'], {foreignKey : 'question'});
+db.entity['Exam'].hasMany(db.entity['ExamQuestion'], {foreignKey: 'exam'});
+db.entity['ExamQuestion'].hasMany(db.entity['ExamStudent'], {foreignKey: 'examQuestion'});
 module.exports = db;
