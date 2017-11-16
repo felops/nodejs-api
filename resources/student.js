b850f7c8cb1f2c22f795eacc792c144cb032a7fe
felops <felops@users.noreply.github.com>
2017-11-16 00:54:07 -0200

    ✨ create the resource student and the option to save the questions from student's exam

---
+++ resources/student.js
@@ -0,0 +1,27 @@
+module.exports = (app, models) => {
+  app.post('/api/student/:student/exam/:exam/question/:question', (req, res) => {
+    if(req.params.question != req.body.nextQuestion) {
+      models.entity['ExamStudent'].create({
+        student: req.params.student,
+        examQuestion: req.body.nextQuestion
+      })
+    }
+
+    if(req.params.question) {
+      models.entity['ExamStudent'].update({
+        dateEnd: models.sequelize.literal('NOW()'),
+        questionOption: req.body.questionOption
+      }, {
+        where: {
+          examQuestion: req.params.question,
+          student: req.params.student
+        }
+      }).then(() => {
+        res.json({
+          data: true,
+          msg: 'Cadastrado com sucesso!'
+        })
+      })
+    }
+  })
+}
