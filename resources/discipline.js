ac420b457efa47e8eddd20c8a4e07bf5b6613836
felops <felops@users.noreply.github.com>
2017-11-19 12:45:20 -0200

    ✨ add resource to get fields from discipline

--- resources/discipline.js
+++ resources/discipline.js
@@ -5,6 +5,16 @@ module.exports = (app, models) => {
     })
   })
 
+  app.get('/api/discipline/:id/field', (req, res) => {
+    models.entity['DisciplineField'].findAll({
+      where: {
+        discipline: req.params.id
+      }
+    }).then((data) => {
+      res.json(data)
+    })
+  })
+
   app.post('/api/discipline', (req, res) => {
     models.entity['Discipline'].create(req.body).then((data) => {
       res.json({
