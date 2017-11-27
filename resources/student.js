93765d2158c409a818d9246fbce80aca3a7607f5
felops <felops@users.noreply.github.com>
2017-11-27 15:24:36 -0200

    🎨 improve the code to save an student answer

--- resources/student.js
+++ resources/student.js
@@ -1,27 +1,15 @@
 module.exports = (app, models) => {
   app.post('/api/student/:student/exam/:exam/question/:question', (req, res) => {
-    if(req.params.question != req.body.nextQuestion) {
-      models.entity['StudentAnswer'].create({
-        student: req.params.student,
-        examQuestion: req.body.nextQuestion
+    models.entity['StudentAnswer'].create({
+      student: req.params.student,
+      examQuestion: req.params.question,
+      questionOption: req.body.questionOption,
+      dateStart: req.body.dateStart
+    }).then(() => {
+      res.json({
+        data: true,
+        msg: 'Cadastrado com sucesso!'
       })
-    }
-
-    if(req.params.question) {
-      models.entity['StudentAnswer'].update({
-        dateEnd: models.sequelize.literal('NOW()'),
-        questionOption: req.body.questionOption
-      }, {
-        where: {
-          examQuestion: req.params.question,
-          student: req.params.student
-        }
-      }).then(() => {
-        res.json({
-          data: true,
-          msg: 'Cadastrado com sucesso!'
-        })
-      })
-    }
+    })
   })
 }
