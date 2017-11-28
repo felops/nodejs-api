705161a1d7db77a8efcb6cc9723aae7df500a04b
felops <felops@users.noreply.github.com>
2017-11-27 23:39:13 -0200

    ✨ resource to get the performance of the student

--- resources/student.js
+++ resources/student.js
@@ -1,4 +1,31 @@
 module.exports = (app, models) => {
+  app.get('/api/student/:student/exam/:exam/resultados', (req, res) => {
+    models.entity['Exam'].findAll({
+      include: [{
+        model: models.entity['ExamQuestion'],
+        include: [
+          {
+            model: models.entity['StudentAnswer'],
+            where: {
+              student: req.params.student
+            }
+          },
+          {
+            model: models.entity['Question'],
+            include: [{
+              model: models.entity['QuestionAnswer']
+            }]
+          }
+        ]
+      }],
+      where: {
+        id: req.params.exam
+      }
+    }).then((data) => {
+      res.json(data)
+    })
+  })
+
   app.post('/api/student/:student/exam/:exam/question/:question', (req, res) => {
     models.entity['StudentAnswer'].create({
       student: req.params.student,
