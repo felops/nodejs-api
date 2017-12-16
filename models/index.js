56ea76b38623b6ae29810d12b7387902a572af27
felops <felops@users.noreply.github.com>
2017-12-16 16:16:07 -0200

    🐛 fix the order of the inicialization of sequelize

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-const sequelize = new Sequelize('ARANDU', 'root', '', {dialect: 'mysql'})
 const fs        = require("fs")
 const path      = require("path")
 const Sequelize = require("sequelize")
+const sequelize = new Sequelize('ARANDU', 'root', '', {dialect: 'mysql'})
 
 let db = {
   entity: {},
